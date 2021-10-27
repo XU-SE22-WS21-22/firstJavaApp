86c31d96043d2caf04c8d254d0d5d6551e62bfa0
Mohammed AbuJarour <m.abujarour@xu-university.de>
2021-10-27 13:28:55 +0200

    add my first java class

---
+++ src/firstJavaProject/FirstClass.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package firstJavaProject;
+
+/**
+ * @author mohammed
+ *
+ */
+public class FirstClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+		System.out.println("This line is coded by Mohammmed");
+
+	}
+
+}
