7cd5acf6c898cd877cfe462fbfef0a28f47a3809
Mohammed AbuJarour <m.abujarour@xu-university.de>
2021-10-27 13:55:22 +0200

    Add authenticator

---
+++ src/firstJavaProject/Authenticator.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package firstJavaProject;
+
+/**
+ * @author mohammed
+ *
+ */
+public class Authenticator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("User authentication is required");
+
+	}
+
+}
